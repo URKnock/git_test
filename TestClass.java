8819886028fbe20001befb0a364246cd41b3e7d3
URKnock <chein1010@naver.com>
2020-01-14 15:49:11 +0900

    add gitignore

--- TestClass.java
+++ TestClass.java
@@ -1,9 +1,11 @@
 package test;
 
-public class TextClass {
-	public static class main(String args[]) {
+public class TestClass {
+	public static void main(String args[]) {
 			System.out.println("hi!");
 			System.out.println("My name is Knock Knock!");
 			// git commit -a -m "text commit"
+			// git log - > commit 번호 6자리 확인 가능
+			// git checkout commit 번호 6자리 입력 -> 그 commit으로 돌아감
 	}
 }
