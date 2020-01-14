f925c534d74993986a9f349046cad6538e99eef6
Knock <chein1010@naver.com>
2020-01-14 16:10:30 +0900

    Change Hi

--- TestClass.java
+++ TestClass.java
@@ -2,7 +2,7 @@ package test;
 
 public class TestClass {
 	public static void main(String args[]) {
-			System.out.println("hi!");
+			System.out.println("Hi everyone!");
 			System.out.println("My name is Knock Knock!");
 			// git commit -a -m "text commit"
 			// git log - > commit 번호 6자리 확인 가능
@@ -10,7 +10,7 @@ public class TestClass {
 			// http://gitignore.io/ 	.gitignore 파일 생성하기
 
 			// git remote add origin https://github.com/URKnock/git_test	저장소와 연결
-			// git push -u origin master									저장소에 저장
+			// git push -u origin master	저장소에 저장
 			// git pull(덮어쓰기), fetch(비교하기)	<- 저장소에서 가져오기
 	}
 }
