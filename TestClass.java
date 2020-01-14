f731e76fe9ee16e579a821999b25e972f28b3d57
URKnock <chein1010@naver.com>
2020-01-14 16:08:31 +0900

    write description

--- TestClass.java
+++ TestClass.java
@@ -8,5 +8,9 @@ public class TestClass {
 			// git log - > commit 번호 6자리 확인 가능
 			// git checkout commit 번호 6자리 입력 -> 그 commit으로 돌아감
 			// http://gitignore.io/ 	.gitignore 파일 생성하기
+
+			// git remote add origin https://github.com/URKnock/git_test	저장소와 연결
+			// git push -u origin master									저장소에 저장
+			// git pull(덮어쓰기), fetch(비교하기)	<- 저장소에서 가져오기
 	}
 }
