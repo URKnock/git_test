e024a759eabcbb7cc32b82f11734f48545e048e8
URKnock <chein1010@naver.com>
2020-01-14 15:16:00 +0900

    first commit in branch

--- TestClass.java
+++ TestClass.java
@@ -2,8 +2,8 @@ package test;
 
 public class TextClass {
 	public static class main(String args[]) {
-			System.out.println("hello!");
-			System.out.println("Knock Knock!");
+			System.out.println("hi!");
+			System.out.println("I'm Knock Knock!");
 			// git commit -a -m "text commit"
 	}
 }
