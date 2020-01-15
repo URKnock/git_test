ee8ce647ac688f6368cdc1a5d921b6113ffc5f6f
URKnock <chein1010@naver.com>
2020-01-15 14:30:45 +0900

    node server test2

--- TestClass.java
+++ TestClass.java
@@ -2,7 +2,7 @@ package test;
 
 public class TestClass {
 	public static void main(String[] args) {
-			System.out.println("Hi EVERYONE!");
+			System.out.println("Hi!");
 			System.out.println("My name is Knock Knock!");
 			// git commit -a -m "text commit"
 			// git log - > commit 번호 6자리 확인 가능
